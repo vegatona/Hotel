796016cfccbbeccfe8f0b77fa67c8f0a50c3fd89
vegatona <oswaldovega756@gmail.com>
2024-11-21 12:00:45 -0700

    1

---
+++ Data/Models/Habitacion.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hotel.Data.Models
+{
+    public class Habitacion
+    {
+        public int id { get; set; }
+        public string Tipo { get; set; }
+        public string Descripcion { get; set; }
+        public string Estado { get; set; }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -1,7 +1,9 @@
+using Hotel.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -16,9 +18,11 @@ namespace Hotel
 {
     public class Startup
     {
+        public string ConnectionString { get; set; }
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+            ConnectionString = Configuration.GetConnectionString("DefaultConnectionString");
         }
 
         public IConfiguration Configuration { get; }
@@ -28,6 +32,8 @@ namespace Hotel
         {
 
             services.AddControllers();
+            //configuracion DbContext con sql
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(ConnectionString));
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Hotel", Version = "v1" });
