151987ee59dd7ab3add81cfe2610dc6b9f09b273
cisco01 <oswaldovega756@gmail.com>
2024-11-22 08:14:14 -0700

    1.1

--- Data/Models/Habitacion.cs
+++ Data/Models/Habitacion.cs
@@ -4,9 +4,6 @@ namespace Hotel.Data.Models
 {
     public class Habitacion
     {
-        public int id { get; set; }
-        public string Tipo { get; set; }
-        public string Descripcion { get; set; }
-        public string Estado { get; set; }
+
     }
 }
